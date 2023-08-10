d2eb03275332ff4386cb2aa28942bd252e9a29dd
“doaajumaa” <doaajumaa966@gmail.com>
2023-08-10 16:17:04 -0700

    com

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main -this is the min function of the c program
- * return : RETURN 0
+ * Return: always 0
  */
 
-int main (void)
-{ 
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
