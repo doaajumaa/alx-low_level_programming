50b5de6384c5833debf2fde9c9586b2ebf2749d2
“doaajumaa” <doaajumaa966@gmail.com>
2023-08-11 18:18:38 -0700

    commit c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - main function
+ *
+ * Return: alwayz 0
+ */
+int main(void)
+{
+char i;
+for (i = 'a' ; i >= 0 ; i++)
+	putchar(i);
+for (i = 'A' ; i >= 0 ; i++)
+	putchar(i);
+putchar('\n');
+	return (0);
+}
