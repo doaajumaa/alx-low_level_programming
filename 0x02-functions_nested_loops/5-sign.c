f1e66ccb86b570bb4fdcf273493d0237bbc8022f
“doaajumaa” <doaajumaa966@gmail.com>
2023-08-15 09:55:44 -0700

    l

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,8 +13,8 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-return (1);
 _putchar(43);
+return (1);
 }
 else if (n == 0)
 {
@@ -23,7 +23,7 @@ return (0);
 }
 else if (n < 0)
 {
-return (-1);
 _putchar(45);
+return (-1);
 }
 }
