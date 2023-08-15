1c1f453e82c45bf15d5e44cdc07a12dd27622fc8
“doaajumaa” <doaajumaa966@gmail.com>
2023-08-15 07:32:57 -0700

    commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _islower - Checks for lowercase 
- * @c: the chat to be checked
+*_islower - Checks for lowercase
+* @c: the chat to be checked
  * Return: 1 for lowecase or 0 for uppercase
  */
- int _islower(int c)
+int _islower(int c)
 {
-if (c >= 97 && c<= 122)
+if (c >= 97 && c <= 122)
 {
 return (1);
 }
