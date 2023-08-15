f3f395b84dd39334f96763522f7831f3536b6e25
“doaajumaa” <doaajumaa966@gmail.com>
2023-08-15 05:18:49 -0700

    comm

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - main function
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+char c;
+for (c = 'a' ; c <= 'z' ; c++)
+putchar(c);
+putchar(\n);
+return (0);
+}
