81700b62c84dbb7a260b4a5fab07147d4939363b
“doaajumaa” <doaajumaa966@gmail.com>
2023-08-17 03:58:29 -0700

    s

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_isdigit - check the code
+ *@c: int to be checked if intiger or not
+ * Return: Returns 1 if c is a digit
+*Returns 0 otherwise
+ */
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+return (0);
+else
+return (1);
+}
