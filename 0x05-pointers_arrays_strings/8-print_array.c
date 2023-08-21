3ee16444a39a70e8248ac1122e43f2a4f4c9b743
“doaajumaa” <doaajumaa966@gmail.com>
2023-08-21 14:14:47 -0700

    ma

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_array - prints any n elements of a
+ * @a: array
+ * @n: the number of elements to print
+ * Return: returns void
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+	printf("%d", a[i]);
+	if (i < n - 1)
+		printf(" ,");
+}
+printf("\n");
+}
