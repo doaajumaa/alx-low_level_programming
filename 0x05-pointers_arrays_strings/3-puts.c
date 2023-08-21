6ccc8db0bfa8286c6ef1afda45b9f8f541f291e5
“doaajumaa” <doaajumaa966@gmail.com>
2023-08-21 10:32:35 -0700

    k

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main"
+/**
+ * _puts - prints str
+ * @str: string to print
+ * Return:returns void
+ */
+void _puts(char *str)
+{
+int s;
+for (s = 0; str[s] != '\0'; s++)
+_putcar str[i];
+_putchar('\n');
+}
