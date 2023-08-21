d34ab6aa89f760f598cde301a30aa3090462ee76
“doaajumaa” <doaajumaa966@gmail.com>
2023-08-21 11:19:16 -0700

    s

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_rev - prinr string in rev
+ *@s: string to print
+ *Return: returns string in revers
+ */
+void print_rev(char *s)
+{
+int x;
+int count;
+for (x = 0; s[x] != '\0'; x++)
+	count++;
+for (x = count - 1 ; x >= 0; x--)
+_putchar(s[x]);
+}
