76b73ec53584de26ed444a683b6f4db29b508d99
“doaajumaa” <doaajumaa966@gmail.com>
2023-08-21 12:12:57 -0700

    m

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,11 +7,11 @@
 void rev_string(char *s)
 {
 int x;
-char y;
 int count = 0;
 for (x = 0; s[x] != '\0'; x++)
 	count++;
 for (x = 0; x >= count / 2; x++)
+char y;
 y == s[x];
 s[x] == s[count - 1 - x];
 s[count - 1 - x] == y;
