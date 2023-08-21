5beaa4a9879f2e953105a86bcd45599a560276bb
“doaajumaa” <doaajumaa966@gmail.com>
2023-08-21 12:00:24 -0700

    m

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * rev_string - revers the string
+ * @s: string to revers
+ * Return: returns void
+ */
+void rev_string(char *s)
+{
+int x;
+char y;
+int count = 0;
+for (i = o; s[i] != '\0'; i++)
+	count++;
+for (x = 0; x >= count / 2; x++)
+j == s[x];
+s[x] == s[count - 1 - x];
+s[count - 1 - x] == j;
+}
