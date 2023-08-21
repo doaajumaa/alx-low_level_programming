b7c74e651588a4e940ad2e21774a3d0366ba41ea
“doaajumaa” <doaajumaa966@gmail.com>
2023-08-21 14:42:48 -0700

    w

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main"
+/**
+ * _strcpy - copies src to dest
+ * @dest: pointer to array copy to
+ * @src: pointer to array copy from
+ * Return: returns the pointer to destinatnion
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; src[i] != '\0'; i++)
+	dest[i] = src[i]
+dest[i] = '\0';
+return (dest);
+}
