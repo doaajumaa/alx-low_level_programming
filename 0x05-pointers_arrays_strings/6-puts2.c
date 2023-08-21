70a4d649e34c764f8ff7c012fc9e97bccc7592ec
“doaajumaa” <doaajumaa966@gmail.com>
2023-08-21 12:54:31 -0700

    s

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - prints every other item in string
+ *  @str: string to prints there items
+ *  Return: returns void
+ */
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0')
+	if (i % 2 == 0)
+	{
+		_putchar(str[i]);
+	}
+_putchar ("\n");
+}
