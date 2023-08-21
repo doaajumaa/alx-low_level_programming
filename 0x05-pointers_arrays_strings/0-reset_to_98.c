658369942051b8fcedeaf0be73d1604efbb82200
“doaajumaa” <doaajumaa966@gmail.com>
2023-08-21 08:34:43 -0700

    e

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,11 @@
 #include "main.h"
-/*reset_to_98 - resert value
+
+/**
+ * reset_to_98 - reserts n to 89
  *@n: the pointer to var
 *Return: void
  */
 void reset_to_98(int *n)
 {
- *n=98;
+*n = 98;
 }
