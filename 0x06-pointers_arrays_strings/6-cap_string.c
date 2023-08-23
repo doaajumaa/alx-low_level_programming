f106730eeacdcb0bb159252bb52d9008b3626268
“doaajumaa” <doaajumaa966@gmail.com>
2023-08-23 09:11:58 -0700

    r

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,16 +1,25 @@
 #include "main.h"
 /**
- * string_toupper - turn lowercase to upper case in s
+ * cap_string  - turn lowercase to upper case in s after delimiter
  * @s: string of chars
  * Return: returns the new string
  */
-char *string_toupper(char *s)
+char *cap_string(char *s)
 {
-int i;
-for (i = 0 ; s[i] != '\0' ; i++)
-	if (s[i] >= 'a' && s[i] <= 'z')
-	{
-		s[i] = s[i] - 32;
-        }
-return (s);
+int i, j;
+char spt[13] = {' ', '\t', '\n'
+	, ',', ';', '.', '!', '?', '"', '(', ')', '{', ')'};
+for (i = 0; s[i] != '\0'; i++)
+{
+if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+	s[i] = s[i] - 32;
+for (j = 0; j < 13; j++)
+{
+	if (s[j] == spt[j])
+		if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+		{
+			s[i + 1] = s[i + 1] - 32;
+		}
+}
+}
 }
