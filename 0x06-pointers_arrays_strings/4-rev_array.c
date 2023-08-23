12c4e001f4d519588532313aa79541a076bb25dc
“doaajumaa” <doaajumaa966@gmail.com>
2023-08-23 12:37:47 -0700

    l

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,11 +9,12 @@ void reverse_array(int *a, int n)
 {
 int i;
 int j;
-for (i = 0 ; i < n ; i++)
+for (i = 0 ; i < n \2; i++)
 {
 j = a[i];
 a[i] = a[n - 1 - i];
 a[n - 1 - i] = j;
 }
-return(0);
+
+
 }
