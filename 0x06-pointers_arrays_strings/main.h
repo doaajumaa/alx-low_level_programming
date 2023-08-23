99f758f9d7371c456dea49aadc9a86c3d4f2291d
“doaajumaa” <doaajumaa966@gmail.com>
2023-08-23 11:35:09 -0700

    n

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+int _strcmp(char *s1, char *s2);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
