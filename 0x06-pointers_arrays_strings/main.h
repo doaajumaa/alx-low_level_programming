7a5abea2d3b3842e41f16d5c93b314ee383bec24
doaajumaa <doaajumaa966@gmail.com>
2023-08-24 11:16:35 -0700

    k

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
