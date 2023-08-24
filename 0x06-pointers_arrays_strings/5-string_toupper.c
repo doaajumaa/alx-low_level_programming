4e1870a4ace22724fcc0942c5dd1daf33253f3e5
doaajumaa <doaajumaa966@gmail.com>
2023-08-24 13:36:01 -0700

    f

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,9 +8,9 @@ char *string_toupper(char *s)
 {
 int i;
 for (i = 0 ; s[i] != '\0' ; i++)
-        if (s[i] >= 'a' && s[i] <= 'z')
-        {
-                s[i] = s[i] - 32;
-        }
+	if (s[i] >= 'a' && s[i] <= 'z')
+	{
+		s[i] = s[i] - 32;
+	}
 return (s);
 }
