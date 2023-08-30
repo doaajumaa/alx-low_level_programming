fd844d63cc556549d03ddf0d6b41247fdbbd5721
doaajumaa <doaajumaa966@gmail.com>
2023-08-30 14:22:06 -0700

    i

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,16 @@
 #includr "main.h"
 /**
- *
+ * _puts_recursion - recursivly puts string
+ * @s: string to put
+ * Return: returns 0
  */
 void _puts_recursion(char *s)
 {
-
+if (*s != '\0')
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+else
+_putchar('\n');
 }
