2d51b877118120e05d37c5e30599f8511356029d
doaajumaa <doaajumaa966@gmail.com>
2023-09-04 13:28:46 -0700

    t

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
  * create_array - reserve loction wih malloc
  * @size: size of arr
  * @c: arr content
- * RETURN: retturns pointer to reserved area
+ * Return: retturns pointer to reserved area
  */
 char *create_array(unsigned int size, char c)
 {
@@ -11,8 +11,8 @@ unsigned int i;
 char *arr;
 if (size == 0)
 	return (NULL);
-arr = malloc(sizeof (char) * size);
-if (arr = NULL)
+arr = malloc(sizeof(char) * size);
+if (arr == NULL)
 	return (NULL);
 for (i = 0; i < size; i++)
 	arr[i] = c;
