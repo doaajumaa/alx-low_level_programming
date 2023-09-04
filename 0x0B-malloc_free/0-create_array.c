93e863d641e177f84073c292f18a8cc0c0f34a59
doaajumaa <doaajumaa966@gmail.com>
2023-09-04 13:19:04 -0700

    t

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * create_array - reserve loction wih malloc
+ * @size: size of arr
+ * @c: arr content
+ * RETURN: retturns pointer to reserved area
+ */
+char *create_array(unsigned int size, char c)
+{
+unsigned int i;
+char *arr;
+if (size == 0)
+	return (NULL);
+arr = malloc(sizeof (char) * size);
+if (arr = NULL)
+	return (NULL);
+for (i = 0; i < size; i++)
+	arr[i] = c;
+return (arr);
+}
