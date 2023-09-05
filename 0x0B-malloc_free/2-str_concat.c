2886ba33ff79819187d7fb2353457ba922cb86a5
doaajumaa <doaajumaa966@gmail.com>
2023-09-05 16:37:37 -0700

    l

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * str_concat - concat two strings
+ * @s1: first str to con
+ * @s2: to conc
+ * Return: return pointer to result
+ */
+char *str_concat(char *s1, char *s2)
+{
+int s1l = 0;
+int s2l = 0;
+int i;
+int *con;
+if (s1 == NULL)
+	s1 = "";
+if (s2 == NUL)
+	s2 = "";
+for (i = 0; s1[i] != '\0'; i++)
+	s1l++;
+for (i = 0; s2[i] != '\0'; i++)
+	s2l++;
+con = malloc(sizeof(char) * (s1l + s2l) + 1);
+if (con == NULL)
+	return (NULL);
+for (i = 0; s1[i] != '\0'; i++)
+	con[i] = s1[i];
+for (i = 0; s2[i] != '\0'; i++)
+	con[s1l + 1] = s2[i];
+return (con);
+}
