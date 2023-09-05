0ed72947184269beeead6cc9c59a52ad620cfaa1
doaajumaa <doaajumaa966@gmail.com>
2023-09-05 16:40:20 -0700

    l

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,10 +10,10 @@ char *str_concat(char *s1, char *s2)
 int s1l = 0;
 int s2l = 0;
 int i;
-int *con;
+char *con;
 if (s1 == NULL)
 	s1 = "";
-if (s2 == NUL)
+if (s2 == NULL)
 	s2 = "";
 for (i = 0; s1[i] != '\0'; i++)
 	s1l++;
