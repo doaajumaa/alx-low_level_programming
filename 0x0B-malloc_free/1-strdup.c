082d6b876b000735960bebb9f1d4b2b5f974451f
doaajumaa <doaajumaa966@gmail.com>
2023-09-05 15:51:01 -0700

    w

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strdup - copy str in memo
+ * @str: string to copy
+ * Return: returns copy from str
+ */
+char *_strdup(char *str)
+{
+int i;
+char *copy;
+int count = 0;
+if (str  == NULL)
+	return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+	count++;
+copy =  malloc(sizeof(char) * count + 1);
+if (copy == NULL)
+	return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+	copy[i] = srt[i];
+return (copy);
+}
