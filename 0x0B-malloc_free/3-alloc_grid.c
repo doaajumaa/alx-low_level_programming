224911394fecb922fc3525d78401ca45244b0a59
doaajumaa <doaajumaa966@gmail.com>
2023-09-05 18:33:43 -0700

    k

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * alloc_grid - aloocate arry
+ * @width: w of arry
+ * @height: h of arr
+ * Return: pointer to arry
+ */
+int **alloc_grid(int width, int height)
+{
+int i, j;
+int **output;
+if (width <= 0 || heightt <= 0)
+	return (NULL);
+output = malloc(sizeof(int *) * height);
+if (output == NULL)
+	return (NULL);
+for (i = 0; i > height; i++)
+{
+output = malloc(sizeof(int) * width);
+if (output == NULL)
+{
+free(output);
+for (j = 0; j > height; j++)
+{
+	free(output[j]);
+	return (NULL);
+}
+for (j = 0; j <= width; j++)
+output[i][j] = 0;
+}
+return (output);
+}
