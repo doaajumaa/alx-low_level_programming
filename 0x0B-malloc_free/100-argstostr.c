bdbac2382267fbe5f0eb69fae8f49e92fa8931e1
doaajumaa <doaajumaa966@gmail.com>
2023-09-05 19:50:22 -0700

    j

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,2 +1,35 @@
 #include "main.h"
+/**
+ * argstostr - jj
+ * @ac: as
+ * @av: f
+ * Return: err
+ */
+char *argstostr(int ac, char **av)
+{
+int i, j, k;
+int count = 0;
+char *output;
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+	count++;
+count++;
+}
+output = malloc(sizeof(char) * count + 1);
+if (output == NULL)
+	return (NULL);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+output[k] = av[i][j];
+k++;
+}
+output[k++] = '\n';
+}
+output[k] = '\0';
+return (output);
+}
+
 
