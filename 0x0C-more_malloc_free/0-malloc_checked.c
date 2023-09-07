66c19db4896ac565f732b6cbe6185dfb5fdd1479
doaajumaa <doaajumaa966@gmail.com>
2023-09-07 03:12:42 -0700

    h

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- *
+ * malloc_checked - j
+ * @b: pointer to
+ * Return: returns
  */
 void *malloc_checked(unsigned int b)
 {
 char *m;
 m = malloc(b);
-f (m = NUUL)
+if (m == NULL)
 	exite(98);
-else 
+else
 	return (b);
 }
